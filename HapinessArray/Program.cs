85f4edacbd80b9f1d801b80bac664b50f09ac0c3
David Reina <davidereina@hotmail.com>
2023-10-06 10:04:41 -0500

    Console app to upgrade each value of an array of number

---
+++ HapinessArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HapinessArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] happiness = new int[] { 1,2,3,4,5 };
+            UpgradeHappiness(happiness);
+            Console.WriteLine("The updated happiness array is: ");
+            foreach (int i in happiness)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void UpgradeHappiness(int[] happiness)
+        {
+            for(int i = 0; i < happiness.Length; i++)
+            {
+                happiness[i] = happiness[i] + 2;
+            }
+        }
+    }
+}
